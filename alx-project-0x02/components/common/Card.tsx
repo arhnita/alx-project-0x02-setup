6a31a2758e138e8241dd0425bf240b6ac11732a7
Anita Samuel <migist2@gmail.com>
2025-07-23 02:33:00 +0100

    props

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,4 +1,5 @@
-import { CardProps } from "@/interfaces";
+import React from 'react';
+import { type CardProps } from '../../interfaces';
 
 const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
