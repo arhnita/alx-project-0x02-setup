e9b977d19cb3b1d4f936e6759d8980ab2e8f5847
Anita Samuel <migist2@gmail.com>
2025-07-24 14:00:38 +0100

    header

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center py-4">
           <Link href="/" className="text-2xl font-bold hover:text-blue-200">
-            Next.js App
+            My App
           </Link>
           <nav>
             <ul className="flex space-x-6">
--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,6 +1,9 @@
+import Header from "@/components/layout/Header";
+
 const Posts: React.FC = () => {
   return (
-    <div className="container p-4">
+    <div>
+        <Header />
       <h1 className="text-3xl text-white">Posts</h1>
     </div>
   );
