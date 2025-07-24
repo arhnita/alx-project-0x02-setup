256f3383264b87c164aea0deca36a610baddaf40
Anita Samuel <migist2@gmail.com>
2025-07-24 15:58:34 +0100

    get static props

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,70 +1,29 @@
 import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
 import { type PostProps } from "@/interfaces";
-import { useState } from "react";
-import { useEffect} from "react";
 
-const Posts: React.FC = () => {
-  const [posts, setPosts] = useState<PostProps[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+export async function getStaticProps(){
+  try{
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await response.json()
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        //start the request
-
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/posts"
-        );
-
-        //check if successful
-        if (!response.ok) {
-          throw new Error("Failed to fetch posts");
-        }
-        //parse json data
-        const data = await response.json();
-
-        //update state with data
-        setPosts(data.slice(0, 12)); //limit to 12 posts
-      } catch (err) {
-        setError("Error fetching posts");
-        console.error("Error:", err);
-      } finally {
-        setLoading(false);
+    return{
+      props: {
+        posts: posts.slice(0,12)
+      },
+      revalidate: 60 //Regenerate every 60 secons
+    }
+  } catch(error){
+    console.error(error)
+    return {
+      props: {
+        posts: []
       }
-    };
-
-    //now mount once
-    fetchPosts();
-  }, []);
-
-  if(loading) {
-    return(
-      <div>
-        <Header />
-        <main className="container mx-auto px-4 py-8">
-          <div className="text-center">
-            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto"></div>
-            <p className="mt-4 text-gray-600">Loading posts...</p>
-          </div>
-        </main>
-      </div>
-    )
+    }
   }
+}
 
-  if(error) {
-    return (
-      <div>
-        <Header />
-        <main className="container mx-auto px-4 py-8">
-          <div className="text-center">
-            <p className="text-red-600">{error}</p>
-          </div>
-        </main>
-      </div>
-    )
-  }
+const Posts: React.FC<{posts: PostProps[]}> = ({ posts }) => {
 
   return (
     <div>
